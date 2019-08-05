e86f0b4c43a0bc527ff9a76ececf16f435ce8086
Alexander Culum <alexander.culum@traxpay.com>
2019-08-05 11:04:25 +0200

    Fix expiration time and latest sequence number state retrieval.

--- src/main/java/dev/jlibra/spring/action/PeerToPeerTransfer.java
+++ src/main/java/dev/jlibra/spring/action/PeerToPeerTransfer.java
@@ -4,6 +4,7 @@ import admission_control.AdmissionControlOuterClass.AdmissionControlStatus;
 import com.google.protobuf.ByteString;
 import dev.jlibra.JLibra;
 import dev.jlibra.KeyUtils;
+import dev.jlibra.admissioncontrol.query.AccountState;
 import dev.jlibra.admissioncontrol.query.ImmutableGetAccountState;
 import dev.jlibra.admissioncontrol.query.ImmutableQuery;
 import dev.jlibra.admissioncontrol.query.UpdateToLatestLedgerResult;
@@ -17,7 +18,9 @@ import org.springframework.stereotype.Component;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Arrays;
 
+import static java.time.Instant.now;
 import static java.util.Arrays.asList;
 
 @Component
@@ -38,7 +41,7 @@ public class PeerToPeerTransfer {
                 .sequenceNumber(fetchLatestSequenceNumber(KeyUtils.toByteArrayLibraAddress(publicKey.getEncoded())))
                 .maxGasAmount(validMaxGasAmount)
                 .gasUnitPrice(validGasUnitPrice)
-                .expirationTime(10000)
+                .expirationTime(now().getEpochSecond() + 10000L)
                 .program(ImmutableProgram.builder().code(ByteString.copyFrom(Move.peerToPeerTransferAsBytes())).addAllArguments(asList(addressArgument, amountArgument)).build()).build();
 
         SubmitTransactionResult result = jLibra.getAdmissionControl().submitTransaction(publicKey, privateKey,
@@ -50,8 +53,14 @@ public class PeerToPeerTransfer {
         UpdateToLatestLedgerResult result = jLibra.getAdmissionControl()
                 .updateToLatestLedger(ImmutableQuery.builder().addAccountStateQueries(ImmutableGetAccountState.builder().address(address).build()).build());
 
-        return result.getAccountStates().get(0).getSequenceNumber();
-
+        return result.getAccountStates()
+                .stream()
+                .filter(accountState -> Arrays.equals(
+                        accountState.getAddress(),
+                        address))
+                .map(AccountState::getSequenceNumber)
+                .findFirst()
+                .orElse(0L);
     }
 
     public static class PeerToPeerTransferReceipt {
